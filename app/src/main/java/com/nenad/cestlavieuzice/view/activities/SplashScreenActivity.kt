0f7057adf8f7690f58f88a2bb04d394ae0bc9bec
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-05-02 18:10:59 +0200

    Initial commit

---
+++ app/src/main/java/com/nenad/cestlavieuzice/view/activities/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.nenad.cestlavieuzice.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nenad.cestlavieuzice.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
